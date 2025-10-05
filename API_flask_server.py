5db43c215e2079b1db15c785dcdaeeb89bd9e7f5
Edmilson Alexandre <145597126+EdAnder32@users.noreply.github.com>
2025-10-05 17:21:40 +0100

    Refactor API response and update run configuration
    Updated the API to return a dictionary in the home route and changed the app to run on host 0.0.0.0 and port 7860.

--- API_flask_server.py
+++ API_flask_server.py
@@ -2,31 +2,28 @@
 ## Design by edander32 (Edmilson Alexandre) and jjambo(Joaquim Jambo)
 
 from flask import Flask, request, jsonify
-import joblib
 import pandas as pd
+import joblib
 
 app = Flask(__name__)
 
 model = joblib.load("exoplanet_model.pkl")
-@app.route("/", methods=["GET"])
+
+@app.route("/")
 def home():
-    return jsonify({"message": "API do classificador de exoplanetas online."})
+    return {"message": "API do classificador de exoplanetas online."}
 
 @app.route("/predict", methods=["POST"])
 def predict():
     try:
-        data = request.get_json(force=True)
+        data = request.get_json()
         df = pd.DataFrame([data])
-
         pred = model.predict(df)[0]
         prob = model.predict_proba(df)[0][1]
-
-        return jsonify({
-            "prediction": int(pred),
-            "probability": float(prob)
-        })
+        return jsonify({"prediction": int(pred), "probability": float(prob)})
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=7860)
+
